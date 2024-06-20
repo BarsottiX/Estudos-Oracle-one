a8517ecaf191c3571bd23fbecd3fb9fe4dd27de2
Washington Barsotti <120425768+BarsottiX@users.noreply.github.com>
2024-06-19 22:22:29 -0300

    Add files via upload

---
+++ app copy.js
@@ -0,0 +1,14 @@
+alert ('Bem vindo ao nosso site!');
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+let Nome = prompt('Digite o seu nome');
+let qualaIdade = prompt ('Digite sua idade');
+if (qualaIdade >= 18){
+    alert('Pode tira a habilitação')
+}
+
---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Boas vindas ao jodo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 30'); //EXIBE MENSAGEM NA TELA
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai! você descobriu o número secreto (5)');
+}
+
